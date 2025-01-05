5cec0aca26fc153026a0766d710d2fc9a390172f
Cornelus793 <Cornelus793@gmail.com>
2025-01-05 10:31:07 +0000

    add pdf et docx fichier autorisé

--- middlewares/uploadPhoto.js
+++ middlewares/uploadPhoto.js
@@ -35,7 +35,7 @@ const upload = multer({
 });
 
 function checkFileType(file, cb) {
-    const allowedFileTypes = /jpeg|jpg|png|svg|webp/;
+    const allowedFileTypes = /jpeg|jpg|png|svg|docx|pdf|webp/;
     const mimetype = allowedFileTypes.test(file.mimetype);
     const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
 
