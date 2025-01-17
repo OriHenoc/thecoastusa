3db3cfb5ee63eeea389d4173bb8841b1b2ea4eaa
Cornelus793 <Cornelus793@gmail.com>
2025-01-17 11:11:21 +0000

    reset pwd

--- controllers/utilisateurController.js
+++ controllers/utilisateurController.js
@@ -483,7 +483,7 @@ exports.resetPasswordRequest = async (req, res) => {
       return res.status(200).json({ message: "Email de réinitialisation envoyé avec succès. \nVeuillez consulter votre email !" });
     } catch (error) {
       console.error('Erreur lors de la demande de réinitialisation :', error);
-      return res.status(500).json({ message: `Erreur serveur. Veuillez réessayer plus tard.` });
+      return res.status(500).json({ message: `Erreur serveur. Veuillez réessayer plus tard.\n ${error} });
     }
   };
   
